2f4bdadb452c708393cfac1f759d59c4d669d6c4
chi-wei-lien <lien2@purdue.edu>
2022-10-14 21:09:32 -0400

    fix no response bug

--- pages/api/table.ts
+++ pages/api/table.ts
@@ -4,14 +4,9 @@ import { table, base } from "../../utils/airtable"
 import { getSession } from 'next-auth/react'
 import { Session } from 'next-auth'
 
-type Data = {
-  title: string,
-  details: string
-}
-
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) => {
   const { method } = req;
   switch(method) {
@@ -19,28 +14,25 @@ const handler = async (
       res.json({ title: 'this is cool', details: 'rocket'});
       break;
     case 'POST':
-      console.log(req.body);;
-      createIdea(req);
+      const session = await getSession({ req })
+      const email = session?.user?.email || "null";
+      base('wishlist').create([
+        {
+          "fields": {
+            "title": req.body.inputs.title,
+            "gmail": email,
+            "details": req.body.inputs.details
+          }
+        },
+      ], function (err: Error) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ msg: err});
+        }
+      });
+      console.log("record added sucessfully");
+      return res.status(200).json({ msg:"record added successfully" });
   }
-}
-
-const createIdea = async (req: NextApiRequest) => {
-  const session = await getSession({ req })
-  const email = session?.user?.email || "null";
-  base('wishlist').create([
-    {
-      "fields": {
-        "title": req.body.inputs.title,
-        "gmail": email,
-        "details": req.body.inputs.details
-      }
-    },
-  ], function(err: Error) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
 }
 
 export default handler
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,6 @@ export default function Home() {
         'Content-Type': 'application/json',
       },
     })
-    console.log(response);
   }
   
   let loginButton;
