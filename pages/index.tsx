29e1b3510c537838e94daf6a2e2a64e74f623ae9
chi-wei-lien <lien2@purdue.edu>
2022-10-14 22:38:01 -0400

    add upside down footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,36 +82,63 @@ export default function Home() {
   const scroll = () => formRef.current!.scrollIntoView({behavior: "smooth"});
 
   return (
-    <div className="h-max bg-amber-50">
-      <div className="grid grid-cols-6 gap-4">
-        <div className="px-3 py-3 font-noto">
-          {loginButton}
-        </div>
-        <div className="items-center justify-center col-span-4 col-start-2 mt-10">
-          <h1 className="text-5xl text-center font-noto">Purdue Hackers' Wishlist. &nbsp;
-          <FontAwesomeIcon
-            icon={faChessKnight}
-            style={{ fontSize: 50}}
-          />
-          </h1>
-          <div className="flex-grow border-t-2 border-black"></div>
-          <div className="flex items-center justify-center w-full mt-5">
-            <button onClick={scroll} className="px-5 py-2 text-white bg-black rounded-full">Start An Idea</button>
+    <div>
+      <div className="h-max bg-amber-50">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="px-3 py-3 font-noto">
+            {loginButton}
           </div>
-          <div className="flex items-center justify-center w-full mt-2">
+          <div className="items-center justify-center col-span-4 col-start-2 mt-10">
+            <h1 className="text-5xl text-center font-noto">Purdue Hackers' Wishlist. &nbsp;
             <FontAwesomeIcon
-              icon={faArrowDown}
-              style={{ fontSize: 25}}
-              className="animate-bounce"
+              icon={faChessKnight}
+              style={{ fontSize: 50}}
             />
+            </h1>
+            <div className="flex-grow border-t-2 border-black"></div>
+            <div className="flex items-center justify-center w-full mt-5">
+              <button onClick={scroll} className="px-5 py-2 text-white bg-black rounded-full">Start An Idea</button>
+            </div>
+            <div className="flex items-center justify-center w-full mt-2">
+              <FontAwesomeIcon
+                icon={faArrowDown}
+                style={{ fontSize: 25}}
+                className="animate-bounce"
+              />
+            </div>
+            <img className="mx-auto mt-5 w-96" src="typewriter.png"></img>
+            <div className="mt-4 mb-12 bg-white rounded-md min-h-fit h-96 drop-shadow-lg font-noto" ref={formRef}>
+              <h3 className="pt-5 text-lg tracking-wide text-center">Help us build Purdue Hackers</h3>
+              {form}
+            </div>
           </div>
-          <img className="mx-auto mt-5 w-96" src="typewriter.png"></img>
-          <div className="mt-4 mb-12 bg-white rounded-md min-h-fit h-96 drop-shadow-lg font-noto" ref={formRef}>
-            <h3 className="pt-5 text-lg tracking-wide text-center">Help us build Purdue Hackers</h3>
-            {form}
+        </div>
+      </div>
+      <div className="rotate-180">
+        <div className="h-30 bg-neutral-700">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="items-center justify-center col-span-4 col-start-2 mt-10">
+            <h1 className="mt-10 text-5xl text-center text-white font-noto">Purdue Hackers' Wishlist. &nbsp;
+            <FontAwesomeIcon
+              icon={faChessKnight}
+              style={{ fontSize: 50}}
+            />
+            </h1>
+            <div className="flex-grow border-t-2 border-white"></div>
+            <div className="flex items-center justify-center w-full mt-5">
+              <button onClick={scroll} className="px-5 py-2 text-black bg-white rounded-full">Start An Idea</button>
+            </div>
+            <div className="flex items-center justify-center w-full mt-2">
+              <FontAwesomeIcon
+                icon={faArrowDown}
+                style={{ fontSize: 25}}
+                className="text-white animate-bounce"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
