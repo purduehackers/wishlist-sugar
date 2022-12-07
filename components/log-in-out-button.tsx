b2cf6df83265b4e76430f54c7b6ff70bc434abf9
chi-wei-lien <lien2@purdue.edu>
2022-12-07 12:17:37 -0500

    better handle displaying username

--- components/log-in-out-button.tsx
+++ components/log-in-out-button.tsx
@@ -3,8 +3,13 @@ import { useSession, signIn, signOut } from "next-auth/react";
 const LogInOutButton = () => {
   const { data: session } = useSession();
   if (session) {
+    if (session.user) {
+      return (
+        <div><p>Hi, {session.user.name}</p> <button onClick={() => signOut()} className="px-3 py-1 text-white bg-black rounded-full">Sign out</button></div>
+      )
+    }
     return (
-      <div><p>Hi, {session.user!.name}</p> <button onClick={() => signOut()} className="px-3 py-1 text-white bg-black rounded-full">Sign out</button></div>
+      <div><button onClick={() => signOut()} className="px-3 py-1 text-white bg-black rounded-full">Sign out</button></div>
     )
   }
   return (
