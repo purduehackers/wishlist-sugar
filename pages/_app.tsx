269e41b1a3929a582af47d9dc6d79ab91809cf1b
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-09-24 14:55:07 -0400

    rename parent folder to purdue_hackers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,21 @@
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import Head from "next/head";
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
-import { createGlobalStyle } from "styled-components";
-import { config, dom } from "@fortawesome/fontawesome-svg-core";
+import { createGlobalStyle } from 'styled-components';
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 const GlobalStyles = createGlobalStyle`
     ${dom.css()}
-`; 
+`;
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
     <div>
       <Head>
-        <link
-            rel='shortcut icon'
-            href="/image/favicon.ico"
-          />
+        <link rel="shortcut icon" href="/image/favicon.ico" />
       </Head>
-      <GlobalStyles/>
+      <GlobalStyles />
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
